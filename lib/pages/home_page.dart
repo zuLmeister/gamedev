3da57e63426a7d6602868ee0f7ef047ca2eb9e3c
zuLmeister <86194101+zuLmeister@users.noreply.github.com>
2022-09-27 23:18:58 +0800

    Add mediaquery height x mediaquery weight

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,20 +16,24 @@ class _HomePageState extends State<HomePage> {
   ];
   @override
   Widget build(BuildContext context) {
+    final mediaQueryHeight = MediaQuery.of(context).size.height;
+    final mediaQueryWidth = MediaQuery.of(context).size.width;
+    final bool landScape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final bodyHeight = mediaQueryHeight - MediaQuery.of(context).padding.top;
     return Scaffold(
       drawer: const NavBar(),
       backgroundColor: const Color.fromARGB(255, 35, 34, 34),
       appBar: AppBar(
-        title: Image.asset('assets/icons/logo.png', height: 70.0),
+        title: Image.asset('assets/icons/logo.png', height: 115),
         centerTitle: true,
         backgroundColor: const Color.fromARGB(255, 35, 34, 34),
       ),
       body: Center(
-        child: ListView(
+        child: (landScape) ? Column (
           children: <Widget>[
             CarouselSlider(
               options: CarouselOptions(
-                height: MediaQuery.of(context).size.width,
+                height: bodyHeight * 0.55,
                 autoPlay: true,
                 viewportFraction: 1,
                 autoPlayInterval: const Duration(seconds: 4),
@@ -40,7 +44,7 @@ class _HomePageState extends State<HomePage> {
                     width: MediaQuery.of(context).size.width,
                     decoration: BoxDecoration(
                         color: const Color.fromARGB(255, 35, 34, 34),
-                        border: Border.all(color: Colors.white, width: 5.0)),
+                        border: Border.all(color: Colors.white, width: mediaQueryWidth)),
                     child: Center(
                       child: Image.asset(i, fit: BoxFit.fill),
                     ),
@@ -48,7 +52,6 @@ class _HomePageState extends State<HomePage> {
                 });
               }).toList(),
             ),
-            const Divider(height: 10,),
             Container(
               alignment: Alignment.center,
               padding: const EdgeInsets.all(10),
@@ -61,24 +64,23 @@ class _HomePageState extends State<HomePage> {
                     color: Colors.white),
               ),
             ),
-            const Divider(height: 10,),
             Wrap(
               alignment: WrapAlignment.spaceAround,
               children: [
                 Image.network(
                   'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
                   width: 115,
-                  height: 90,
+                  height: bodyHeight * 0.25,
                 ),
                 Image.network(
                   'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
                   width: 115,
-                  height: 90,
+                  height: bodyHeight * 0.25,
                 ),
                 Image.network(
                   'https://oflutter.com/wp-content/uploads/2021/02/girl-profile.png',
                   width: 115,
-                  height: 90,
+                  height: bodyHeight * 0.25,
                 ),
               ],
             ),
